c41e5ac1604025a5f0b49954f0cd39983388d706
Ruslan Gula <ruslan3gula@gmail.com>
2022-10-02 22:05:06 +0300

    simple api processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [searchUrl, setSearchUrl] = useState<string>("");
+  const updateSearch = (e: any) => {
+    setSearchUrl(e.target.value);
+  };
+
+  let user = "";
+  let repo = "";
+
+  const processSearchUrl = () => {
+    const result = searchUrl.split("/");
+    user = result[3];
+    repo = result[4];
+    fetch(`https://api.github.com/repos/${user}/${repo}/issues`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input type="text" className="text" onChange={(e) => updateSearch(e)} />
+      <button onClick={processSearchUrl}>Load issues</button>
     </div>
   );
 }
