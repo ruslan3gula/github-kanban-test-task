675302495746f837ee8d529795549da8aad7b500
Ruslan Gula <ruslan3gula@gmail.com>
2022-10-24 22:09:52 +0300

    api connected to boards

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,142 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { DragList } from './components/DragList';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { DragList } from "./components/DragList";
 
 function App() {
-  const [searchUrl, setSearchUrl] = useState<string>('');
+  const [searchUrl, setSearchUrl] = useState<string>("");
   const updateSearch = (e: any) => {
     setSearchUrl(e.target.value);
   };
 
-  let user = '';
-  let repo = '';
+  let user = "";
+  let repo = "";
+
+  const [boards, setBoards] = useState<any>([]);
 
   const processSearchUrl = () => {
-    const result = searchUrl.split('/');
+    const result = searchUrl.split("/");
     user = result[3];
     repo = result[4];
     fetch(`https://api.github.com/repos/${user}/${repo}/issues`)
-      .then(res => res.json())
-      .then(data => console.log(data));
+      .then((res) => res.json())
+      // .then((data) => console.log(data))
+      .then((value) => filterIssues(value))
+      .then((value) => setBoards(value));
+  };
+
+  const [currentBoard, setCurrentBoard] = useState<{
+    id: number;
+    title: string;
+    items: [
+      {
+        id: string;
+        title: string;
+      }
+    ];
+  } | null>(null);
+
+  const [currentItem, setCurrentItem] = useState<{
+    id: string;
+    title: string;
+  } | null>(null);
+
+  const onDragStartHandler = (e: any, board: any, item: any) => {
+    setCurrentBoard(board);
+    setCurrentItem(item);
+    console.log("debug > onDragStartHandler===", item);
+  };
+
+  const onDragEndHandler = (e: any, board: any, item: any) => {
+    e.target.style.boxShadow = "none";
+    console.log("debug > onDragEndHandler ===", item);
+  };
+
+  const onDropHandler = (e: any, board: any, item: any) => {
+    console.log("debug > onDropHandler===", item);
+
+    e.preventDefault();
+    e.stopPropagation();
+    if (currentBoard && currentItem) {
+      const currentIndex = currentBoard.items.indexOf(currentItem);
+
+      currentBoard.items.splice(currentIndex, 1);
+
+      const dropIndex = board.items.indexOf(item);
+
+      const offset =
+        board === currentBoard && currentIndex === dropIndex ? 1 : 0;
+
+      board.items.splice(dropIndex + offset, 0, currentItem);
+      setBoards(
+        boards.map((b: any) => {
+          if (b.id === board.id) {
+            return board;
+          }
+          if (b.id === currentBoard.id) {
+            return currentBoard;
+          }
+          return b;
+        })
+      );
+    }
+  };
+
+  const onDropBoardHandler = (e: any, board: any) => {
+    e.preventDefault();
+    if (currentBoard && currentItem) {
+      const currentIndex = currentBoard.items.indexOf(currentItem);
+
+      currentBoard.items.splice(currentIndex, 1);
+
+      board.items.push(currentItem);
+      setBoards(
+        boards.map((b: any) => {
+          if (b.id === board.id) {
+            return board;
+          }
+          if (b.id === currentBoard.id) {
+            return currentBoard;
+          }
+          return b;
+        })
+      );
+    }
   };
 
   return (
     <div className="container">
-      <input type="text" className="text" onChange={e => updateSearch(e)} />
+      <input type="text" className="text" onChange={(e) => updateSearch(e)} />
       <button onClick={processSearchUrl}>Load issues</button>
-      <DragList />
+
+      <DragList
+        boards={boards}
+        setBoards={setBoards}
+        onDragStartHandler={onDragStartHandler}
+        onDragEndHandler={onDragEndHandler}
+        onDropHandler={onDropHandler}
+        onDropBoardHandler={onDropBoardHandler}
+      />
     </div>
   );
 }
 
 export default App;
+
+interface TIssues {
+  issues: [];
+}
+
+const filterIssues = (data: TIssues) => {
+  console.log("data", data);
+  const result: any = [
+    { id: 1, title: "OPEN", items: data },
+    { id: 2, title: "IN PROGRESS", items: [] },
+    {
+      id: 3,
+      title: "DONE",
+      items: [],
+    },
+  ];
+  return result;
+};
--- src/components/DragList.tsx
+++ src/components/DragList.tsx
@@ -1,132 +1,47 @@
-import React, { useState } from 'react';
-import './DragList.css';
+import React, { useState } from "react";
+import "./DragList.css";
 
-export const DragList = () => {
-  const [boards, setBoards] = useState([
-    { id: 1, title: 'OPEN', items: [{ id: 1, title: 'Мішок' }] },
-    { id: 2, title: 'IN PROGRESS', items: [{ id: 1, title: 'Свиня' }] },
-    {
-      id: 3,
-      title: 'DONE',
-      items: [
-        { id: 1, title: 'Кінь' },
-        { id: 2, title: 'Тісто' },
-        { id: 3, title: 'Паляниця' },
-      ],
-    },
-  ]);
-
-  const [currentBoard, setCurrentBoard] = useState<{
-    id: number;
-    title: string;
-    items: [
-      {
-        id: string;
-        title: string;
-      }
-    ];
-  } | null>(null);
-
-  const [currentItem, setCurrentItem] = useState<{
-    id: string;
-    title: string;
-  } | null>(null);
-
+export const DragList = ({
+  boards,
+  setBoards,
+  onDragStartHandler,
+  onDragEndHandler,
+  onDropBoardHandler,
+  onDropHandler,
+}: any) => {
   const onDragOverHandler = (e: any, board: any, item: any) => {
-    console.log('debug > onDragOverHandler===', item);
+    console.log("debug > onDragOverHandler===", item);
 
     e.preventDefault();
-    if (e.target.className == 'item') {
-      e.target.style.boxShadow = '0 4px 3px grey';
-      e.target.style.border = 'solid 2px red';
+    if (e.target.className == "item") {
+      e.target.style.boxShadow = "0 4px 3px grey";
+      e.target.style.border = "solid 2px red";
     }
   };
 
   const onDragLeaveHandler = (e: any) => {
-    e.target.style.boxShadow = 'none';
-    e.target.style.border = '2px solid lightpink';
-  };
-
-  const onDragStartHandler = (e: any, board: any, item: any) => {
-    setCurrentBoard(board);
-    setCurrentItem(item);
-    console.log('debug > onDragStartHandler===', item);
-  };
-
-  const onDragEndHandler = (e: any, board: any, item: any) => {
-    e.target.style.boxShadow = 'none';
-    console.log('debug > onDragEndHandler ===', item);
-  };
-
-  const onDropHandler = (e: any, board: any, item: any) => {
-    console.log('debug > onDropHandler===', item);
-
-    e.preventDefault();
-    e.stopPropagation();
-    if (currentBoard && currentItem) {
-      const currentIndex = currentBoard.items.indexOf(currentItem);
-
-      currentBoard.items.splice(currentIndex, 1);
-
-      const dropIndex = board.items.indexOf(item);
-
-      const offset = board === currentBoard && currentIndex === dropIndex ? 1 : 0;
-
-      board.items.splice(dropIndex + offset, 0, currentItem);
-      setBoards(
-        boards.map(b => {
-          if (b.id === board.id) {
-            return board;
-          }
-          if (b.id === currentBoard.id) {
-            return currentBoard;
-          }
-          return b;
-        })
-      );
-    }
-  };
-
-  const onDropBoardHandler = (e: any, board: any) => {
-    e.preventDefault();
-    if (currentBoard && currentItem) {
-      const currentIndex = currentBoard.items.indexOf(currentItem);
-
-      currentBoard.items.splice(currentIndex, 1);
-
-      board.items.push(currentItem);
-      setBoards(
-        boards.map(b => {
-          if (b.id === board.id) {
-            return board;
-          }
-          if (b.id === currentBoard.id) {
-            return currentBoard;
-          }
-          return b;
-        })
-      );
-    }
+    e.target.style.boxShadow = "none";
+    e.target.style.border = "2px solid lightpink";
   };
 
   return (
     <div className="wrapp">
-      {boards.map(board => (
+      {boards.map((board: any) => (
         <div
           className="board"
-          onDragOver={e => onDragOverHandler(e, null, null)}
-          onDrop={e => onDropBoardHandler(e, board)}
+          onDragOver={(e) => onDragOverHandler(e, null, null)}
+          onDrop={(e) => onDropBoardHandler(e, board)}
         >
           <div className="board_title">{board.title}</div>
-          {board.items.map(item => (
+          {board.items.map((item: any) => (
             <div
               className="item"
               draggable="true"
-              onDragOver={e => onDragOverHandler(e, board, item)}
-              onDragLeave={e => onDragLeaveHandler(e)}
-              onDragStart={e => onDragStartHandler(e, board, item)}
-              onDragEnd={e => onDragEndHandler(e, board, item)}
-              onDrop={e => onDropHandler(e, board, item)}
+              onDragOver={(e) => onDragOverHandler(e, board, item)}
+              onDragLeave={(e) => onDragLeaveHandler(e)}
+              onDragStart={(e) => onDragStartHandler(e, board, item)}
+              onDragEnd={(e) => onDragEndHandler(e, board, item)}
+              onDrop={(e) => onDropHandler(e, board, item)}
             >
               {item.id} {item.title}
             </div>
